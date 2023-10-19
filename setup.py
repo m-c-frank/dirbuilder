7b807a82361457710decae02160e2a4b5e0c25b8
Martin Christoph Frank <martin7.frank7@gmail.com>
2023-10-19 21:08:36 +0200

    made setuptools better

--- setup.py
+++ setup.py
@@ -4,13 +4,16 @@ setup(
     name='dirbuilder',
     version='0.2',
     packages=find_packages(),
+    entry_points={
+        'console_scripts': ['dirbuilder=dirbuilder.__main__:main'],
+    },
     install_requires=[
         'openai',
-        'python-dotenv'
+        'dotenv'
     ],
-    entry_points={
-        'console_scripts': [
-            'dirbuilder=dirbuilder.create_directories:main',
-        ],
-    },
+    url='https://github.com/m-c-frank/dirbuilder',
+    license='GOS License',
+    author='Martin Christoph Frank',
+    author_email='martin7.frank7@gmail.com',
+    description='A utility to translate structured text into directory structures.'
 )
