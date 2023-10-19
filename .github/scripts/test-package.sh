3b43d05cfdad82d08d82b43314a1fe4435298496
Martin Christoph Frank <martin7.frank7@gmail.com>
2023-10-19 22:21:32 +0200

    learning about args

--- .github/scripts/test-package.sh
+++ .github/scripts/test-package.sh
@@ -3,14 +3,8 @@
 # Exit immediately if a command exits with a non-zero status.
 set -e
 
-# Your bash commands to test the package
-# For example:
-# dirbuilder [input_file_path] [output_directory_path]
+# Check dirbuilder's help message
+dirbuilder -h
 
-# If dirbuilder is a command-line utility after installing the package
-dirbuilder --version
-
-# Or if you want to run python and then test:
-# python -c 'import dirbuilder; print(dirbuilder.__version__)'
-
+# Print a success message
 echo "Package seems to work as expected!"
