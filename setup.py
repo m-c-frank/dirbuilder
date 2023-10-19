9485289d8ac2fcea04c823e9de9c61fa64c7a882
Martin Christoph Frank <martin7.frank7@gmail.com>
2023-10-19 20:40:43 +0200

    adding entry point to make it runnable

--- setup.py
+++ setup.py
@@ -2,10 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name='dirbuilder',
-    version='0.1',
+    version='0.2',
     packages=find_packages(),
     install_requires=[
         'openai',
         'python-dotenv'
     ],
+    entry_points={
+        'console_scripts': [
+            'dirbuilder=dirbuilder.create_directories:main',
+        ],
+    },
 )
